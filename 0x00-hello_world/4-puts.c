908d5ec9dbb70664b2138e6c43f2aab92735be00
S4m1-maker <siphosamimasuku@gmail.com>
2023-05-18 23:08:04 +0000

    0x00. C - Hello, World Project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Is the parent function of puts
+ *
+ *Return: returns the value 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function is the parent function
+ *
+ * Return: returns the value 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
