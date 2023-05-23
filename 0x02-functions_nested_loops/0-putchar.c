48954f8abb8fe42214885c5ac28704d47cc55231
S4m1-maker <siphosamimasuku@gmail.com>
2023-05-23 20:26:43 +0000

    Updating _putchar program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-*main - prints the output: _putchar
+* main - prints out the characters _putchar
+* @c: the parametr of the function
 *
-*Return: returns 0 when successful
+* Return: returns 0 when successful
 */
 
 int print_char(char c);
@@ -20,9 +21,15 @@ int main(void)
 
 
 
+/**
+* print_char - writes the cahracter c to stdout
+* @c: the parametr of the function
+*
+* Return: returns the parameter c when successful
+*/
 
 int print_char(char c)
- {
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
