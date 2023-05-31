290357a7efe6636a0649464c755bfa630f3545fd
S4m1-maker <siphosamimasuku@gmail.com>
2023-05-31 01:10:22 +0000

    Function that updates the value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates the value of a pointer to 98
+* @n: is the pointer parameter to update
+*
+* Return: success when the program executes
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
