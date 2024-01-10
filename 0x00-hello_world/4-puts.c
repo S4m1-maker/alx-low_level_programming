1e66d024193e5ce33e37ff6e0fd5dc7ad0c43eb0
Default User (with sudo) <s4m1-maker@github.com>
2024-01-10 12:03:16 -0500

    program tha prints a string of text followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to stdout followed by a newline
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+}
