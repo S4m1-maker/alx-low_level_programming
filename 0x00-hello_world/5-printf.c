3182ed8d4cc8ebe82b8726a9259afe129a12b9c2
Default User (with sudo) <s4m1-maker@github.com>
2024-01-11 04:43:20 -0500

    program that prints a string using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string by calling the printf function
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
