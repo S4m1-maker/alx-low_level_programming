1b77e96473d4730da16010840fbd5429bc6e4cdf
Default User (with sudo) <s4m1-maker@github.com>
2024-01-26 11:22:01 -0500

    function that prints 10 times the alphabet, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: return is void
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph, lineNum = 0;
+
+	while (lineNum < 10)
+	{
+		for (alph = 'a' ; alph <= 'z' ; alph++)
+		{
+			_putchar(alph);
+		}
+		_putchar('\n');
+		_putchar(lineNum);
+		lineNum++;
+	}
+}
