36757ffc2a8c186f355ae6e45aef7504ac10b5c4
Default User (with sudo) <s4m1-maker@github.com>
2024-01-26 10:32:46 -0500

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the name _putchar
+ *
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	printname();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+void printname(void);
+
+
+
+#include <stdio.h>
+void printname(void)
+{
+	printf("_putchar\n");
+}
