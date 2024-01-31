ce3377d34bc8ca212cea4224b4f5383c4dc729f5
Default User (with sudo) <s4m1-maker@github.com>
2024-01-31 05:44:17 -0500

    funtion that prits the 9 times table, starting from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,16 @@ void times_table(void)
 		for (j = 0 ; j <= n ; j++)
 		{
 			k = i * j;
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 
 			if (j != n)
 			{
