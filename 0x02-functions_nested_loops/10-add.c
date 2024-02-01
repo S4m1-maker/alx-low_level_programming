4c31b71980459129dd00221eeebd2e17e3a88fe2
Default User (with sudo) <s4m1-maker@github.com>
2024-02-01 02:06:48 -0500

    function that adds two numbers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: is the first parameter
+ * @b: is the second parameter
+ *
+ * Return: returns the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
