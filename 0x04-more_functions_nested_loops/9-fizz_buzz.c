eb5d0c7738f3ca1a39898a3415f20061dfff7cdd
Default User (with sudo) <s4m1-maker@github.com>
2024-02-13 08:05:07 -0500

    Program that prints the numbers, from 1 to 100 and replaces the multiples of three with the word "Fizz" and multiples of five with the word "Buzz"

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,13 @@ int main(void)
 
 	for (i = 1 ; i <= n ; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		int mult3 = i % 3 == 0, mult5 = i % 5 == 0;
+
+		if (mult3 && mult5)
 			printf("FizzBuzz ");
-		else if (i % 3 == 0)
+		else if (mult3)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if (mult5)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
