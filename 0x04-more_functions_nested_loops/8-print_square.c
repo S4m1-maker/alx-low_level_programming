f133500fc15f536ee4fcac92f3b0afe6bd153569
Default User (with sudo) <s4m1-maker@github.com>
2024-02-13 04:38:16 -0500

    function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: is the parameter size of the square
+ *
+ * Return: return is void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+	char c = '#';
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			if (size <= 0)
+				_putchar('\n');
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
