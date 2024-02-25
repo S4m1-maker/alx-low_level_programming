6d482312945cdd936de245202d8a13ba70fe7597
Default User (with sudo) <s4m1-maker@github.com>
2024-02-25 14:00:44 -0500

    Fucntion that updates the value of the parameter to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the poiter parameter to 98
+ * @n: is the pointer parameter
+ *
+ * Return: return is void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
