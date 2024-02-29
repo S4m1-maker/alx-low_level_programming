e52d73e34ccd9cc55cf53dd8ff1243601ffdc14d
Default User (with sudo) <s4m1-maker@github.com>
2024-02-29 13:03:19 -0500

    Function that prints every other character of a string, starting with the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,8 +14,11 @@ void puts2(char *str)
 
 	for (i = 0 ; *str != '\0' ; i++)
 	{
-		_putchar(*str);
-		str += 2;
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
 	}
 	_putchar('\n');
 }
