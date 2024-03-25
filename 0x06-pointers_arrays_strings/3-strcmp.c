c55f5780a852ea9f0fcd077f14138d8a20c90c08
Default User (with sudo) <s4m1-maker@github.com>
2024-03-25 04:04:26 -0400

    Fucntion that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i;
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
-		;
-	for (j = 0 ; s2[j] != '\0' ; j++)
-		;
-
-	if (*s1 != *s2)
-		return (*s1 - *s2);
+	for (i = 0 ; (s1[i] != '\0') && (s2[i] != '\0') ; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
 	return (0);
 }
