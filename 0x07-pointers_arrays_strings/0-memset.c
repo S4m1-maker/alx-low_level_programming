a7fb92bd51dcc66a7ead32facb8f69a58b84669b
Default User (with sudo) <s4m1-maker@github.com>
2024-04-15 12:07:27 -0400

    Fucntion that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: is the pointer to char in memory parameter
+ * @n: is the n bytes of the memory to fill
+ * @b: is the const byte to be filled
+ *
+ * Return: returns a poiter to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (s);
+}
