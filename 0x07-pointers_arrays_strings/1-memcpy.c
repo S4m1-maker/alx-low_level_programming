2dff6235fc552eb43af566f72ebe72e8e47c117d
Default User (with sudo) <s4m1-maker@github.com>
2024-04-15 13:23:53 -0400

    Fucntion that copies an area in memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies an area of memory
+ * @dest: is the destination memory to copy to
+ * @src: is the source memory to copy from
+ * @n: is the n bytes of memory to copy
+ *
+ * Return: returns a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+
+	while (n--)
+	{
+		*dest++ = *src++;
+	}
+	return (ptr);
+}
