3b44b5d945e052127ab0f76ebaabcb492967bcd8
Default User (with sudo) <s4m1-maker@github.com>
2024-04-17 18:38:13 -0400

    Fucntion that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
-		if (*s == c)
-			return (s);
 		s++;
 	}
 
-	return (0);
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
 }
