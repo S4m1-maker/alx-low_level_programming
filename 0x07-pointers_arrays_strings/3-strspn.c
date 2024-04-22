1a14b2f35ef467734cc83c2cb139dfc6c415a8e3
Default User (with sudo) <s4m1-maker@github.com>
2024-04-22 09:49:38 -0400

    Function that calculates the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strspn - counts the length of a prefix substring
+ * @s: is the string parameter to calculate
+ * @accept: is the string parameter of characters to count in s
+ *
+ * Return: returns the number of bytes in the intial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *ptr = s;
+
+	while (ptr && _strchr(accept, *ptr))
+	{
+		ptr++;
+	}
+
+	return (ptr - s);
+}
+
+
+
+/**
+ * _strchr - locates a character in a string
+ * @s: is the string parameter
+ * @c: is the character to locate
+ *
+ * Return: return a pointer to s or NULL if the character is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+	{
+		s++;
+	}
+
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
+}
